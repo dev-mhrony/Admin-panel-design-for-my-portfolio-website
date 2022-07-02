84050f604897bc18ac2ba2894d1ab03842d51672
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-07-03 02:59:50 +0600

    first commit

---
+++ Include/sidebar.php
@@ -0,0 +1,34 @@
+<div class="sidebar pe-4 pb-3">
+    <nav class="navbar bg-secondary navbar-dark">
+        <a href="index.html" class="navbar-brand mx-4 mb-3">
+            <h3 class="text-primary"><i class="bi bi-tree-fill me-2"></i>Ghasful</h3>
+        </a>
+
+        <div class="navbar-nav w-100">
+            <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="bi bi-briefcase me-2"></i>Qualification</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="education.php" class="dropdown-item">Education</a>
+                    <a href="typography.html" class="dropdown-item">Work Experience</a>
+                </div>
+            </div>
+            <a href="widget.html" class="nav-item nav-link"><i class="bi bi-chat-left-dots-fill me-2"></i>Message</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="bi bi-briefcase me-2"></i>Experience</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="button.html" class="dropdown-item">Frontend Devloper</a>
+                    <a href="typography.html" class="dropdown-item">UI / UX Designer</a>
+                    <a href="typography.html" class="dropdown-item">Backend Devloper</a>
+                </div>
+            </div>
+            <a href="form.html" class="nav-item nav-link"><i class="bi bi-bar-chart-fill me-2"></i>Offer</a>
+            <a href="table.html" class="nav-item nav-link"><i class="bi bi-arrow-clockwise me-2"></i>Recent Work</a>
+            <a href="chart.html" class="nav-item nav-link"><i class="bi bi-emoji-smile-fill me-2"></i>Happy Client</a>
+            <a href="chart.html" class="nav-item nav-link"><i class="bi bi-person-circle me-2"></i>About</a>
+            <a href="chart.html" class="nav-item nav-link"><i class="bi bi-share-fill me-2"></i>Social Media</a>
+            <a href="chart.html" class="nav-item nav-link"><i class="bi bi-chevron-double-down me-2"></i>Footer</a>
+            <a href="chart.html" class="nav-item nav-link"><i class=" me-2">&copy;</i> CopyRight</a>
+        </div>
+    </nav>
+</div>
